76bcc142ff73b19a9905bc4a5f5a93567f761b58
sean <SCOWAN@TCD.IE>
2016-02-21 20:23:27 -0800

    yes

--- test.cpp
+++ test.cpp
@@ -64,9 +64,9 @@ int main(int argc, char** argv) {
                 num_elements_per_proc, MPI_CHAR, 0, MPI_COMM_WORLD);
 
     // Compute the average of your subset
-    for (char var : sub_rand_chars)
+    for (int i = 0; i< num_elements_per_proc; i++)
     {
-        std::cout<<var<<" ";
+        std::cout<<sub_rand_chars[i]<<" ";
     }
     std::cout<<" endline"<<std::endl;
     int sub_avg = remove_a(sub_rand_chars, num_elements_per_proc);
